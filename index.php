64c95afd877d0d27358b9187551556c1acdca2c1
Mike Goren <mike@mortalwombat.net>
2018-01-07 14:46:02 -0800

    add basic html

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" rel="stylesheet" type="text/css">
+  <link href="css/styles.css" rel="stylesheet" type="text/css">
+  <title>Address Book</title>
+</head>
+<body>
+  <div class="container">
+    <h2>megaband t-shirts</h2>
+		<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+			<label>T-shirt (XS)</label>
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="SWZ6QN7PHHJV4">
+			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+			<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+		</form>
+		<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+			<label>T-shirt (S)</label>
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="MCFNMV37RJVL6">
+			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+			<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+		</form>
+		<form target="paypal" action="https://www.paypal.com/cgi-bin/webscr" method="post">
+			<label>T-shirt (M)</label>
+			<input type="hidden" name="cmd" value="_s-xclick">
+			<input type="hidden" name="hosted_button_id" value="3N9QDF82DXW4A">
+			<input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_cart_SM.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+			<img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+		</form>
+  </div>
+</body>
+</html>
