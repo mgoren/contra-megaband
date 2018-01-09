24785dece8110bcdc59da69f8659febbad6d3501
Mike Goren <mike@mortalwombat.net>
2018-01-09 00:24:27 -0800

    updates following meeting

--- app.rb
+++ app.rb
@@ -12,10 +12,18 @@ get '/tickets/' do
   erb :tickets
 end
 
+get '/soldout' do
+  erb :soldout
+end
+
 get '/tshirts' do
   erb :tshirts
 end
 
 get '/tshirts/' do
   erb :tshirts
+end
+
+get '/test' do
+  erb :test
 end
